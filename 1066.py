963c9a105c68eeff52c83772b92ecf45f5846d95
Diogo Costa <diogo.henrique.cdl@gmail.com>
2023-04-18 21:33:31 -0300

    Add 1066 - Pares, Impares, Positivos e Negativos

---
+++ 1066.py
@@ -0,0 +1,21 @@
+par = imp = pos = neg = 0
+
+for i in range(5):
+    n = int(input())
+    if n % 2 == 0:
+        if n > 0:
+            pos += 1
+        elif n < 0:
+            neg += 1
+        par += 1
+    else:
+        if n > 0:
+            pos += 1
+        else:
+            neg += 1
+        imp += 1
+
+print(f"{par} valor(es) par(es)")
+print(f"{imp} valor(es) impar(es)")
+print(f"{pos} valor(es) positivo(s)")
+print(f"{neg} valor(es) negativo(s)")
